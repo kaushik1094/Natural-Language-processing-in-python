67ef3f820bd1e02e34ef7f882f70f5441045da61
KAUSHIK THALLAPALLY <tkaushik20@gmail.com>
2018-02-08 18:27:13 -0500

    Natural Language Processing

---
+++ NLTKsvm.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 15:29:34 2018
+
+@author: kaush
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier= SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train,y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#Accuracy Prediction
+TP =70
+TN = 74
+FP = 23
+FN = 33
+A = (TP + TN) / (TP + TN + FP + FN)
+#precision Prediction
+P = TP / (TP + FP)
+#Recall Prediction
+R = TP / (TP + FN)
+#F1 Score 
+F1= 2 * P * R / (P + R)
